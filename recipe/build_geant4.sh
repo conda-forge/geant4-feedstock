2f80492cc7de50b448d14465cfa31d646db730b7
Chris Burr <christopher.burr@cern.ch>
2020-09-14 08:35:04 +0200

    Update to 10.6.2 and simplify

--- recipe/build_geant4.sh
+++ recipe/build_geant4.sh
@@ -8,15 +8,12 @@ else
   CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
-if [[ ${geant4_visualisation_variant} == "qt" ]]; then
-  CMAKE_PLATFORM_FLAGS+=(-DGEANT4_USE_QT=ON)
-  if [[ ${HOST} =~ .*darwin.* ]]; then
-    CMAKE_PLATFORM_FLAGS+=(-DGEANT4_USE_OPENGL_X11=OFF)
-    CMAKE_PLATFORM_FLAGS+=(-DGEANT4_USE_RAYTRACER_X11=OFF)
-  else
-    CMAKE_PLATFORM_FLAGS+=(-DGEANT4_USE_OPENGL_X11=ON)
-    CMAKE_PLATFORM_FLAGS+=(-DGEANT4_USE_RAYTRACER_X11=ON)
-  fi
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  CMAKE_PLATFORM_FLAGS+=(-DGEANT4_USE_OPENGL_X11=OFF)
+  CMAKE_PLATFORM_FLAGS+=(-DGEANT4_USE_RAYTRACER_X11=OFF)
+else
+  CMAKE_PLATFORM_FLAGS+=(-DGEANT4_USE_OPENGL_X11=ON)
+  CMAKE_PLATFORM_FLAGS+=(-DGEANT4_USE_RAYTRACER_X11=ON)
 fi
 
 if [[ ${DEBUG_C:-no} == yes ]]; then
@@ -33,9 +30,9 @@ sed -r -i -E 's#cmake_minimum_required\(VERSION [0-9]\.[0-9]#cmake_minimum_requi
 mkdir geant4-build
 cd geant4-build
 
-cmake -LAH                                                     \
-      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}                   \
-      -DCMAKE_INSTALL_PREFIX=${PREFIX}                         \
+cmake                                                          \
+      -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}"                 \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}"                       \
       -DGEANT4_BUILD_CXXSTD=17                                 \
       -DGEANT4_USE_SYSTEM_EXPAT=ON                             \
       -DGEANT4_USE_SYSTEM_ZLIB=ON                              \
@@ -43,13 +40,15 @@ cmake -LAH                                                     \
       -DBUILD_SHARED_LIBS=ON                                   \
       -DGEANT4_INSTALL_EXAMPLES=ON                             \
       -DGEANT4_INSTALL_DATA=OFF                                \
-      -DGEANT4_BUILD_MULTITHREADED=OFF                         \
+      -DGEANT4_BUILD_MULTITHREADED=ON                          \
       -DGEANT4_USE_GDML=ON                                     \
-      ${CMAKE_PLATFORM_FLAGS[@]} \
-      ${SRC_DIR}
+      -DGEANT4_USE_QT=ON                                       \
+      -DGEANT4_USE_FREETYPE=ON                                 \
+      "${CMAKE_PLATFORM_FLAGS[@]}"                             \
+      "${SRC_DIR}"
 
-make -j${CPU_COUNT} ${VERBOSE_CM:-}
-make install -j${CPU_COUNT}
+make "-j${CPU_COUNT}" ${VERBOSE_CM:-}
+make install "-j${CPU_COUNT}"
 
 # Print the contents of geant4.sh in case of problems
 echo "Contents of ${PREFIX}/bin/geant4.sh is"
