221a91d76434987c3efc624e249607245082b396
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2026-04-21 12:58:33 +0200

    Continue cleanup of build_geant4.sh

--- recipe/build_geant4.sh
+++ recipe/build_geant4.sh
@@ -30,48 +30,37 @@ fi
 #sed -r -i -E 's#cmake_minimum_required\(VERSION [0-9]\.[0-9]#cmake_minimum_required(VERSION 3.1#gI' \
 #  $(find examples -name 'CMakeLists.txt')
 
-echo "Checking qmake6 availability"
-ls -l "${PREFIX}/bin/qmake6"
-which qmake6
+test -f "${SRC_DIR}/src/CMakeLists.txt"
 
 mkdir geant4-build
-#cd geant4-build
 
 cmake \
     -B ./geant4-build \
-    -S "${SRC_DIR}" \
+    -S "${SRC_DIR}/src" \
     ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON                                   \
+    -DCMAKE_CXX_STANDARD=17                                  \
+    -DGEANT4_BUILD_MULTITHREADED=ON                          \
+    -DGEANT4_BUILD_TLS_MODEL=global-dynamic                  \
+    -DGEANT4_INSTALL_DATA=OFF                                \
+    -DGEANT4_INSTALL_DATADIR="${PREFIX}/share/Geant4/data"   \
+    -DGEANT4_INSTALL_EXAMPLES=ON                             \
+    -DGEANT4_INSTALL_PACKAGE_CACHE=OFF                       \
+    -DGEANT4_USE_FREETYPE=ON                                 \
+    -DGEANT4_USE_GDML=ON                                     \
+    -DGEANT4_USE_QT=ON                                       \
+    -DGEANT4_USE_HDF5=ON                                     \
+    -DGEANT4_USE_SYSTEM_CLHEP=ON                             \
+    -DGEANT4_USE_SYSTEM_EXPAT=ON                             \
+    -DGEANT4_USE_SYSTEM_ZLIB=ON                              \
+    -DQT_QMAKE_EXECUTABLE="${PREFIX}/bin/qmake6"             \
+    "${CMAKE_PLATFORM_FLAGS[@]}"
 
 cmake --build ./geant4-build --config Release --parallel ${CPU_COUNT:-2}
 cmake --install ./geant4-build
-
-cmake ${CMAKE_ARGS}                                            \
-      -DCMAKE_INSTALL_LIBDIR=lib                               \
-      -DCMAKE_BUILD_TYPE=Release                               \
-      -DCMAKE_INSTALL_PREFIX="${PREFIX}"                       \
-      -DBUILD_SHARED_LIBS=ON                                   \
-      -DCMAKE_CXX_STANDARD=17                                  \
-      -DGEANT4_BUILD_MULTITHREADED=ON                          \
-      -DGEANT4_BUILD_TLS_MODEL=global-dynamic                  \
-      -DGEANT4_INSTALL_DATA=OFF                                \
-      -DGEANT4_INSTALL_DATADIR="${PREFIX}/share/Geant4/data"   \
-      -DGEANT4_INSTALL_EXAMPLES=ON                             \
-      -DGEANT4_INSTALL_PACKAGE_CACHE=OFF                       \
-      -DGEANT4_USE_FREETYPE=ON                                 \
-      -DGEANT4_USE_GDML=ON                                     \
-      -DGEANT4_USE_QT=ON                                       \
-      -DGEANT4_USE_HDF5=ON                                     \
-      -DGEANT4_USE_SYSTEM_CLHEP=ON                             \
-      -DGEANT4_USE_SYSTEM_EXPAT=ON                             \
-      -DGEANT4_USE_SYSTEM_ZLIB=ON                              \
-      -DQT_QMAKE_EXECUTABLE="${PREFIX}/bin/qmake6"             \
-      "${CMAKE_PLATFORM_FLAGS[@]}"
-
-make "-j${CPU_COUNT}" ${VERBOSE_CM:-}
-make install "-j${CPU_COUNT}"
 
 # Print the contents of geant4.sh in case of problems
 echo "Contents of ${PREFIX}/bin/geant4.sh is"
